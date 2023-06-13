ca1107c56d940e8b043a461e60cb24da0f24baef
aamiriqbal071 <39021017+aamiriqbal071@users.noreply.github.com>
2023-06-13 16:00:48 +0530

    Add files via upload

---
+++ Word Replacement Program/word_replacement.py
@@ -0,0 +1,7 @@
+def replaceword():
+
+    str = input("enter a Sentence:")
+    word_to_replace = input("enter the word to be replaced from that sentense:")
+    word_replacement= input("enter the word that should be written insted:")
+    print(str.replace(word_to_replace,word_replacement))
+replaceword()  
